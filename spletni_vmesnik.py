22e6517b71550c39b6a0db961b8b444b799b22a7
Gaja Jamnik <gaja.jamnik@gmail.com>
2020-10-11 01:04:56 +0200

    zacetna stran poslje na home page

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -10,6 +10,10 @@ except FileNotFoundError:
     zbirka = ZbirkaPredavanj()
 
 @bottle.get('/')
+def zacetna_stran():
+    bottle.redirect('/preden-pozabim/')
+
+@bottle.get('/preden-pozabim/')
 def home():
     zbirka.dodaj_v_ponavljanja()
     return bottle.template('home.html', zbirka=zbirka)
@@ -19,17 +23,20 @@ def dodaj():
     predmet = bottle.request.forms.getunicode('predmet')
     tema = bottle.request.forms.getunicode('tema')
     zbirka.dodaj_predavanje(predmet, tema)
+    zbirka.shrani_stanje(DATOTEKA_S_STANJEM)
     bottle.redirect('/')
 
 @bottle.post('/pobrisi/<predmet>/<tema>/')
 def pobrisi(predmet, tema):
     zbirka.odstrani_predavanje(predmet, tema)
+    zbirka.shrani_stanje(DATOTEKA_S_STANJEM)
     bottle.redirect('/')
 
 @bottle.post('/oceni/<predmet>/<tema>/')
 def oceni(predmet, tema):
     uspesnost = bottle.request.forms['ocena']
     zbirka.ponovi_iz_ponavljanja(predmet, tema, uspesnost)
+    zbirka.shrani_stanje(DATOTEKA_S_STANJEM)
     bottle.redirect('/')
 
 
