1647ff0f853c753f0d0b880e8018e018e30cc7b9
Gaja Jamnik <gaja.jamnik@gmail.com>
2020-10-10 22:49:07 +0200

    dodane osnovne funkcije v spletni vmesnik

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,16 +1,36 @@
 import bottle
-from datetime import date
+from datetime import *
 from model import Uporabnik, ZbirkaPredavanj, Predavanje, Ponovi
 
-#treba je definirat zbirko predavanj za vsakega uporabnika posebej
-# zbirka_predavanj = Uporabnik.zbirka_predavanj()
+DATOTEKA_S_STANJEM = 'test.json'
 
-zbirka = ZbirkaPredavanj()
-zbirka.dodaj_predavanje('analiza', 'vrste')
+try:
+    zbirka = ZbirkaPredavanj.nalozi_stanje(DATOTEKA_S_STANJEM)
+except FileNotFoundError:
+    zbirka = ZbirkaPredavanj()
 
 @bottle.get('/')
-def osnovna_stran():
-    return bottle.template('osnovna_stran.tpl')
+def home():
+    zbirka.dodaj_v_ponavljanja()
+    return bottle.template('home.html', zbirka=zbirka)
+
+@bottle.post('/dodaj/')
+def dodaj():
+    predmet = bottle.request.forms['predmet']
+    tema = bottle.request.forms['tema']
+    zbirka.dodaj_predavanje(predmet, tema)
+    bottle.redirect('/')
+
+@bottle.post('/pobrisi/<predmet>/<tema>/')
+def pobrisi(predmet, tema):
+    zbirka.odstrani_predavanje(predmet, tema)
+    bottle.redirect('/')
+
+@bottle.post('/oceni/<predmet>/<tema>/')
+def oceni():
+    uspesnost = bottle.request.forms['ocena']
+    zbirka.ponovi_iz_ponavljanja(predmet, tema, uspesnost)
+    bottle.redirect('/')
 
 
 bottle.run(debug=True, reloader=True)
