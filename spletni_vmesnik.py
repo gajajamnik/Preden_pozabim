2e86518bb3a07af9a83771c9336482550e6ed9e0
Gaja Jamnik <gaja.jamnik@gmail.com>
2020-10-11 03:11:39 +0200

    dodana prijava

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -4,29 +4,45 @@ import random
 import os
 from model import Uporabnik, ZbirkaPredavanj, Predavanje, Ponovi
 
+uporabniki = {
+    'gaja': Uporabnik('gaja', '123', ZbirkaPredavanj())
+}
+
 zbirke = {}
 for ime_datoteke in os.listdir('shranjene_zbirke'):
-    st_uporabnika, koncnica = os.path.splitext(ime_datoteke)
-    zbirke[st_uporabnika] = ZbirkaPredavanj.nalozi_stanje(os.path.join('shranjene_zbirke', ime_datoteke))
+    uporabnisko_ime, koncnica = os.path.splitext(ime_datoteke)
+    zbirke[uporabnisko_ime] = ZbirkaPredavanj.nalozi_stanje(os.path.join('shranjene_zbirke', ime_datoteke))
+
+def trenutni_uporabnik():
+    uporabnisko_ime = bottle.request.get_cookie('uporabnisko_ime')
+    if uporabnisko_ime is None:
+        bottle.redirect('/prijava/')
+    return uporabniki[uporabnisko_ime]
 
 # se ne shranjenemu uporabniku doda novo zbirko, shranjenemu pa vrne svojo zbirko
 def zbirka_uporabnika():
-    st_uporabnika = bottle.request.get_cookie('st_uporabnika')
-    if st_uporabnika is None:
-        st_uporabnika = str(random.randint(0, 2 ** 40))
-        zbirke[st_uporabnika] = ZbirkaPredavanj()
-        bottle.response.set_cookie('st_uporabnika', st_uporabnika, path='/')
-    return zbirke[st_uporabnika]
+    return trenutni_uporabnik().zbirka
 
 def shrani_zbirko_uporabnika():
-    st_uporabnika = bottle.request.get_cookie('st_uporabnika')
-    zbirka = zbirke[st_uporabnika]
-    zbirka.shrani_stanje(os.path.join('shranjene_zbirke', f'{st_uporabnika}.json'))
+    uporabnik = trenutni_uporabnik()
+    uporabnik.zbirka.shrani_stanje(os.path.join('shranjene_zbirke', f'{uporabnisko_ime}.json'))
 
 
 @bottle.get('/')
 def zacetna_stran():
     bottle.redirect('/preden-pozabim/')
+
+@bottle.get('/prijava/')
+def prijava_get():
+    return bottle.template('prijava.html')
+
+@bottle.post('/prijava/')
+def prijava_post():
+    uporabnisko_ime = bottle.request.forms['uporabnisko_ime']
+    geslo = bottle.request.forms['geslo']
+    uporabnik = uporabniki[uporabnisko_ime]
+    uporabnik.preveri_geslo(geslo)
+    bottle.response.set_cookie('uporabnisko_ime', uporabnisko_ime, path='/')
 
 @bottle.get('/preden-pozabim/')
 def home():
